6238116d136ae9a115ae9d11ae30c334b66a98b5
SahilKolekar <112554795+SahilKolekar@users.noreply.github.com>
2022-12-15 22:55:29 +0530

    code Added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,23 @@
 package com.driver;
 
 
+
 public class Main {
-   System.out.println("nksjdnkj");
+   static class Product{
+      public int product(int x, int y) {
+         return x*y;
+      }
+      public int product(int x, int y, int z) {
+         return x*y*z;
+      }
+      public double product(double x, double y) {
+         return x*y;
+      }
+   }
+   public static void main(String[] args) {
+      Product p=new Product();
+      p.product(4,6);
+      p.product(1,2,6);
+      p.product(3.5,2.3);
+   }
 }
